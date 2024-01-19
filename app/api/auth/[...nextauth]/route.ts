ad1f47f30462fd63bc429436b46b61e6b00db61f
EsanSamuel <esansamuel555@gmail.com>
2024-01-18 16:19:14 -0800

    fixed some eslint issues and nextauth issues

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,53 +1,6 @@
-import NextAuth, { AuthOptions } from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-import connectDB from "../../../../libs/connect";
-import User from "../../../../models/user";
+import NextAuth from "next-auth";
+import { authOptions } from "../../../../libs/session";
 
-interface Profile {
-  name: string;
-  email: string;
-  picture: string;
-}
-
-const handler: AuthOptions = NextAuth({
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    }),
-  ],
-  callbacks: {
-    async session({ session }) {
-      //get the user's email
-      const LoggedInUserSession = await User.findOne({
-        email: session.user.email,
-      });
-      //define session.user.id
-      session.user.id = LoggedInUserSession._id.toString();
-
-      return session;
-    },
-    async signIn({ profile }: { profile: Profile }) {
-      try {
-        await connectDB();
-
-        const existingUser = await User.findOne({ email: profile.email });
-
-        if (!existingUser) {
-          User.create({
-            username: profile.name,
-            image: profile.picture,
-            email: profile.email,
-          });
-        }
-
-        return true;
-      } catch (error) {
-        console.log(error);
-        return false;
-      }
-    },
-  },
-});
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
---
+++ libs/session.ts
@@ -0,0 +1,52 @@
+import { AuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import connectDB from "../libs/connect";
+import User from "../models/user";
+
+interface Profile {
+  name: string;
+  email: string;
+  picture: string;
+}
+
+export const authOptions: AuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+  callbacks: {
+    async session({ session }) {
+      //get the user's email
+      const LoggedInUserSession = await User.findOne({
+        email: session.user.email,
+      });
+      //define session.user.id
+      session.user.id = LoggedInUserSession._id.toString();
+
+      return session;
+    },
+    async signIn({ profile }: { profile: Profile }) {
+      try {
+        await connectDB();
+
+        const existingUser = await User.findOne({ email: profile.email });
+
+        if (!existingUser) {
+          User.create({
+            username: profile.name,
+            image: profile.picture,
+            email: profile.email,
+          });
+        }
+
+        console.log('Signed in!')
+        return true;
+      } catch (error) {
+        console.log(error);
+        return false;
+      }
+    },
+  },
+};
