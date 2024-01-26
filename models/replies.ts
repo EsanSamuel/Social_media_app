ff6f4393abfe9017a612f4a07126b93c2d76cf26
Esan Samuel <esansamuel555@gmail.com>
2024-01-26 12:16:27 -0800

    Update replies.ts

--- models/replies.ts
+++ models/replies.ts
@@ -1,6 +1,6 @@
-import mongoose, { Schema, models, model } from "mongoose";
+import mongoose, { Schema, models, model, Document } from "mongoose";
 
-interface IReply {
+interface IReply extends Document {
   poster: mongoose.Types.ObjectId;
   comment: mongoose.Types.ObjectId;
   reply: string;
