d8561bb5dcb79de9d677655992886260fde013a5
Esan Samuel <esansamuel555@gmail.com>
2024-01-28 16:45:25 -0800

    Update route.ts

--- app/api/bio/[id]/route.ts
+++ app/api/bio/[id]/route.ts
@@ -33,3 +33,22 @@ const ImageUrl = await cloudinary.uploader.upload(coverImage)
     return new Response("Something went wrong!", { status: 500 });
   }
 };
+
+export const PATCH = async (request: NextRequest, { params }: Params) => { 
+   const { bio ,coverImage } = await request.json(); 
+ const ImageUrl = await cloudinary.uploader.upload(coverImage) 
+   try { 
+     await connectDB(); 
+     const user = await User.findById(params.id); 
+     if (!user) { 
+       return new Response("User not found!", { status: 404}); 
+     } 
+     user.bio = bio; 
+     user.coverImage = ImageUrl.url 
+     await user.save() 
+     return new Response(JSON.stringify(user), { status: 201 }); 
+   } catch (error) { 
+     console.log(error); 
+     return new Response("Something went wrong!", { status: 500 }); 
+   } 
+ };
